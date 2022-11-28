425a770f6810741e72008ade3238cce4dd65183b
jian.jiang <jian.jiang@adicon.com.cn>
2022-11-28 11:45:30 +0900

    服务注册

--- common/src/main/java/demo/web/config/DateParseConfig.java
+++ common/src/main/java/demo/web/config/DateParseConfig.java
@@ -8,8 +8,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 public class DateParseConfig implements WebMvcConfigurer {
@@ -17,7 +20,7 @@ public class DateParseConfig implements WebMvcConfigurer {
 	/** 用于转换表单参数中的日期 */
 	static class DateConverter implements Converter<String, Date> {
 		@Override
-		public Date convert(String source) {
+		public Date convert(  String source) {
 			return DateHelper.parse(source);
 		}
 	}
